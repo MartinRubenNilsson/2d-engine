8a8efc35144bd3bddaa4d971fa5dd986dfa991c0
MartinRubenNilsson <martin.ruben.nilsson@gmail.com>
2025-10-17 22:13:39 +0200

    Refac

--- platform.h
+++ platform.h
@@ -1,6 +1,26 @@
 #pragma once
 
 namespace platform {
+	int system(const char* command);
+	bool set_environment_variable(const char* name, const char* value);
+	bool open(const char* path);
+
+	bool is_debugger_present();
+	void debug_break();
+	void output_debug_string(const char* string);
+
+	// A handle to a dynamic-link library (DLL).
+	struct Library {
+		uintptr_t ptr = 0;
+	};
+
+	Library load_library(const char* lib_file_name);
+
+	// An address to an exported library procedure (function).
+	using LibraryProc = const void*;
+
+	LibraryProc get_library_proc(Library lib, const char* proc_name);
+
 	void start_watching_directory_changes(const std::wstring& directory_path, bool watch_subdirectories = false);
 	void stop_watching_directory_changes(const std::wstring& directory_path);
 	void update_directory_changes();
@@ -22,24 +42,4 @@ namespace platform {
 	std::span<const DirectoryChange> get_directory_changes();
 
 	void shutdown();
-
-	int system(const char* command);
-	bool set_environment_variable(const char* name, const char* value);
-	bool open(const char* path);
-
-	bool is_debugger_present();
-	void debug_break();
-	void output_debug_string(const char* string);
-
-	// A handle to a dynamic-link library (DLL).
-	struct Library {
-		uintptr_t ptr = 0;
-	};
-
-	Library load_library(const char* lib_file_name);
-
-	// An address to an exported library procedure (function).
-	using LibraryProc = const void*;
-
-	LibraryProc get_library_proc(Library lib, const char* proc_name);
 }
--- platform_windows.cpp
+++ platform_windows.cpp
@@ -14,6 +14,44 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 }
 
 namespace platform {
+	int system(const char* command) {
+		return ::system(command);
+	}
+
+	bool set_environment_variable(const char* name, const char* value) {
+		return SetEnvironmentVariableA(name, value);
+	}
+
+	bool _shell_execute_succeeded(HINSTANCE result) {
+		// https://learn.microsoft.com/en-us/windows/win32/api/shellapi/nf-shellapi-shellexecutea#return-value
+		return (INT_PTR)result > 32;
+	}
+
+	bool open(const char* path) {
+		HINSTANCE result = ShellExecuteA(nullptr, "open", path, nullptr, nullptr, SW_SHOWNORMAL);
+		return _shell_execute_succeeded(result);
+	}
+
+	bool is_debugger_present() {
+		return IsDebuggerPresent();
+	}
+
+	void debug_break() {
+		DebugBreak();
+	}
+
+	void output_debug_string(const char* string) {
+		OutputDebugStringA(string);
+	}
+
+	Library load_library(const char* lib_file_name) {
+		return { .ptr = (uintptr_t)LoadLibraryA(lib_file_name) };
+	}
+
+	LibraryProc get_library_proc(Library lib, const char* proc_name) {
+		return GetProcAddress((HMODULE)lib.ptr, proc_name);
+	}
+
 	struct DirectoryChangeContext {
 		std::wstring directory_path;
 		bool watch_subdirectories = false;
@@ -144,44 +182,6 @@ namespace platform {
 
 	void shutdown() {
 		_clear_directory_change_contexts();
-	}
-
-	int system(const char* command) {
-		return ::system(command);
-	}
-
-	bool set_environment_variable(const char* name, const char* value) {
-		return SetEnvironmentVariableA(name, value);
-	}
-
-	bool _shell_execute_succeeded(HINSTANCE result) {
-		// https://learn.microsoft.com/en-us/windows/win32/api/shellapi/nf-shellapi-shellexecutea#return-value
-		return (INT_PTR)result > 32;
-	}
-
-	bool open(const char* path) {
-		HINSTANCE result = ShellExecuteA(nullptr, "open", path, nullptr, nullptr, SW_SHOWNORMAL);
-		return _shell_execute_succeeded(result);
-	}
-
-	bool is_debugger_present() {
-		return IsDebuggerPresent();
-	}
-
-	void debug_break() {
-		DebugBreak();
-	}
-
-	void output_debug_string(const char* string) {
-		OutputDebugStringA(string);
-	}
-
-	Library load_library(const char* lib_file_name) {
-		return { .ptr = (uintptr_t)LoadLibraryA(lib_file_name) };
-	}
-
-	LibraryProc get_library_proc(Library lib, const char* proc_name) {
-		return GetProcAddress((HMODULE)lib.ptr, proc_name);
 	}
 }
 
