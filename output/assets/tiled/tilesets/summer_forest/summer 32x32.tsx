450d6162aa1f274b2f91f0f6eb7c016d56e97bed
MartinRubenNilsson <martin.ruben.nilsson@gmail.com>
2025-10-28 07:22:46 +0100

    Work on stonehenge map

--- output/assets/tiled/tilesets/summer_forest/summer 32x32.tsx
+++ output/assets/tiled/tilesets/summer_forest/summer 32x32.tsx
@@ -1,4 +1,11 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="summer 32x32" tilewidth="32" tileheight="32" tilecount="7" columns="7">
+<tileset version="1.10" tiledversion="1.11.2" name="summer 32x32" tilewidth="32" tileheight="32" tilecount="7" columns="7">
  <image source="summer 32x32.png" width="224" height="32"/>
+ <tile id="2">
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="2" y="6.54545" width="27.8182" height="27.8182">
+    <ellipse/>
+   </object>
+  </objectgroup>
+ </tile>
 </tileset>
